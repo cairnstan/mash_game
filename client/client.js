1db44d11e016c7e589868fb4e08beb8d3ee2c78a
cairnstan <cairnsolsen@gmail.com>
2016-05-03 13:57:17 -0500

    Selects elements from the home.html to be utilized for other views.

--- client/client.js
+++ client/client.js
@@ -32,9 +32,55 @@ app.controller('GameController', function(){
 
 app.controller('HomeController', function(){
   var home = this;
-  home.rules = "M.A.S.H. is a future imagining game. First you will choose your categories."
+  home.category = '';
+  home.categories = [];
+  //home.initialCategories = [home.pet, home.love, home.job];
+  home.initialCategories = [];
+  home.pet = {name: 'pet'};
+  home.love = {name: 'love'};
+  home.job = {name: 'job'};
+  home.biography = {name: 'biography'};
+  home.superpower = {name: 'superpower'};
+  home.live = {name: 'live'};
+  home.spiritAnimal = {name: 'spiritAnimal'};
+  home.adventure = {name: 'adventure'};
+  home.movie = {name: 'movie'};
+  home.campaign = {name: 'campaign'};
+
+
+
+  home.rules = "M.A.S.H. is a future imagining game. First you will choose your categories. Then you will enter items for each category. A magic number will be chosen to cycle through all the items, crossing off the element whenever it gets to the magic number. This continues until there is only one item left in each category. These elements combined will be your story."
+////////psuedocode/////////
+
   home.enterInputs = function(){
+  //   if(home.category == true){
+  //     home.category.push(home.categories);
+  // }
+  home.initialCategories.push(home.pet);
+  home.initialCategories.push(home.job);
+  home.initialCategories.push(home.love);
+  home.initialCategories.push(home.biography);
+  home.initialCategories.push(home.superpower);
+  home.initialCategories.push(home.live);
+  home.initialCategories.push(home.spiritAnimal);
+  home.initialCategories.push(home.adventure);
+  home.initialCategories.push(home.movie);
+  home.initialCategories.push(home.campaign);
+
+  // console.log('initialCategories', home.initialCategories);
+  home.categories = home.initialCategories.filter(function(category){
+    if(category.val == true){
+      return true;
+    }else {
+      return false;
+    }
+  })
+  // console.log(home.category);
+    console.log(home.categories);
     console.log('Your categories have been selected!');
+    // $http.post('/categories', home.category).then(function()){
+    //
+    // }
   }
 });
 
--- server/server.js
+++ server/server.js
@@ -1,33 +1,31 @@
 var express = require('express');
+var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
 var app = express();
 var port = process.env.PORT || 3000;
 
-////////////import modules//////////
+////////////development modules//////////
 var index = require('./routes/index');
 
-
+////////MONGO/////////
+// var mongoURI = 'mongodb://localhost/mash';
+// var MongoDB = mongoose.connect(mongoURI).connection;
+//
+// MongoDB.on('error', function(err){
+//   console.log('MongoDBconnection error:', err);
+// })
+// MongoDB.once('open', function(){
+//   console.log('MongoDB connection open.');
+// })
 
 //////////// config /////////////
 app.use(express.static('server/public'));
 app.use(bodyParser.json());
-
-
-
+app.use(bodyParser.urlencoded({extended: false}));
 
 ///////////routes/////////////
 app.use('/', index);
-
-
-
-
-
-
-
-
-
-
 
 //listen
 app.listen(port, function() {
